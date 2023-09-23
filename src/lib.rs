de374ed6fa9a764fa736d39f11de238ff67a49e5
rimgnyw <94633794+rimgnyw@users.noreply.github.com>
2023-09-23 16:29:26 +0200

    change get_possible_moves. Fail to imagine Sisyphus happy

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::{fmt, vec};
+use std::{fmt, usize, vec};
 
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub enum GameState {
@@ -28,10 +28,10 @@ enum Piece {
 }
 
 impl Piece {
-///////////////////////////////// CHANGE THIS TO BE IN GAME AND ALSO TO WORK WITH THE NOTATION FUNCTION/////////////////////////////////////
+    ///////////////////////////////// CHANGE THIS TO BE IN GAME AND ALSO TO WORK WITH THE NOTATION FUNCTION/////////////////////////////////////
     /// If a piece is standing on the given tile, return all possible
-    /// new positions of that piece. 
-    pub fn get_possible_moves(&self, _position: &str) -> Option<Vec<String>> {
+    /// new positions of that piece.
+    /* pub fn get_possible_moves(&self, _position: &str) -> Option<Vec<String>> {
         // reminder: position is "<file><rank>"
         // there's probably a better solution
         let position = _position
@@ -40,7 +40,8 @@ impl Piece {
             .map(|num| num.parse::<i32>().unwrap())
             .collect::<Vec<i32>>();
         let (file, rank) = (position[0], position[1]);
-        match &self { // this would look better if you just subtracted by 8 instead
+        match &self {
+            // this would look better if you just subtracted by 8 instead
             Piece::King(_colour) => {
                 let mut legal_moves: Vec<String> = vec![];
                 if (rank - 1) * 8 + file > 0 {
@@ -81,8 +82,8 @@ impl Piece {
             _ => None,
             // Piece::Queen => /*...*/,
         }
-    }
-    
+    } */
+
     pub fn get_colour(&self) -> Colour {
         match *self {
             Piece::King(colour)
@@ -108,18 +109,25 @@ impl Game {
     pub fn new() -> Game {
         let mut bboard: [Option<Piece>; 64] = [None; 64]; // needs better name
 
-        let board_template = [
-            "RNBKQBNR",
-            "PPPPPPPP",
-            "********",
-            "********",
-            "********",
-            "********",
-            "PPPPPPPP",
-            "RNBKQBNR ",
-        ];
+        let board_template = "RNBKQBNRPPPPPPPP********************************PPPPPPPPRNBKQBNR "; // imagine a new line every 8 characters
 
-        let mut current_colour = Colour::Black; // 0 = black, 1 = white, might need to change this to include starting position idk
+        let mut current_colour = Colour::White; // 0 = black, 1 = white, might need to change this to include starting position idk
+
+        for i in 0..64 {
+            bboard[i] = match board_template.chars().nth(i).unwrap() {
+                'R' => Some(Piece::Rook(current_colour)),
+                'N' => Some(Piece::Knight(current_colour)),
+                'B' => Some(Piece::Bishop(current_colour)),
+                'K' => Some(Piece::King(current_colour)),
+                'Q' => Some(Piece::Queen(current_colour)),
+                'P' => Some(Piece::Pawn(current_colour)),
+                _ => {
+                    current_colour = Colour::White;
+                    None
+                }
+            }
+        }
+        /*
 
         for (rank, rank_str) in board_template.iter().enumerate() {
             // get the rank intself and the index of the rank
@@ -142,7 +150,7 @@ impl Game {
                     bboard[index] = Some(piece);
                 }
             }
-        }
+        }*/
 
         Game {
             /* initialise board, set active colour to white, ... */
@@ -153,13 +161,15 @@ impl Game {
         }
     }
 
-    pub fn convert_notation(notation: &str)->(&str, &str){ // notation should be <from position><to position> e.g. e1e2 moves the piece at e1 to e2
-        let notation_vector = notation.split_inclusive(char::is_numeric).collect::<Vec<&str>>();
+    pub fn convert_notation(notation: &str) -> (&str, &str) {
+        // notation should be <from position><to position> e.g. e1e2 moves the piece at e1 to e2
+        let notation_vector = notation
+            .split_inclusive(char::is_numeric)
+            .collect::<Vec<&str>>();
 
-        let (from, to) = (notation_vector[0],notation_vector[1]);
+        let (from, to) = (notation_vector[0], notation_vector[1]);
 
         return (from, to);
-    
     }
 
     /// If the current game state is `InProgress` and the move is legal,
@@ -178,9 +188,128 @@ impl Game {
         self.state
     }
 
-    
+    pub fn get_possible_moves(&self, _position: &str) -> Option<Vec<String>> {
+        // reminder: position is "<file><rank>"
+        // there's probably a better solution
+        /* let position = _position
+        .split(|c| c == '<' || c == '>')
+        .filter(|s| !s.is_empty())
+        .map(|num| num.parse::<i32>().unwrap())
+        .collect::<Vec<i32>>(); */
 
+        let _position = _position
+            .split_inclusive(char::is_alphabetic)
+            .collect::<Vec<&str>>();
+        let rank = _position[1].parse::<i32>().unwrap() - 1;
+        let file = match _position[0] {
+            // board is backwards
+            "a" => 7,
+            "b" => 6,
+            "c" => 5,
+            "d" => 4,
+            "e" => 3,
+            "f" => 2,
+            "g" => 1,
+            "h" => 0,
+            _ => panic!(),
+        };
 
+        let position = rank * 8 + file;
+
+        let position = rank * 8 + file;
+        match self.board[position as usize] {
+            // this would look better if you just subtracted by 8 instead
+            Some(Piece::King(Colour::White)) => {
+                let mut legal_moves: Vec<String> = vec![];
+                // 1 step back
+                if position - 1 * 8 > 0 {
+                    legal_moves.push(format!("{}", position - 8));
+
+                    // 1 step back-left
+                    if (rank - 2) * 8 < position - 1 * 8 - 1 {
+                        // check if target position is within the previous line
+                        legal_moves.push(format!("{}", position - 8 - 1));
+                    }
+                    // 1 step back-right
+                    if position - 8 + 1 > (rank - 1) * 8 {
+                        legal_moves.push(format!("{}", position - 8 + 1));
+                    }
+                }
+                // 1 step forward
+                if position + 8 < 8 * 8 {
+                    legal_moves.push(format!("{}", position + 8));
+
+                    // 1 step forward-left
+                    if position + 8 - 1 > (rank + 1) * 8 {
+                        legal_moves.push(format!("{}", position + 8 - 1));
+                    }
+                    // 1 step forward-right
+                    if position + 8 + 1 < (rank + 2) * 8 - 1 {
+                        legal_moves.push(format!("{}", position + 8 + 1));
+                    }
+                }
+
+                // 1 step left
+                if position - 1 > (rank - 1) * 8 {
+                    legal_moves.push(format!("{}", position - 1));
+                }
+                // 1 step right
+                if position + 1 < (rank + 1) * 8 {
+                    legal_moves.push(format!("{}", position + 1));
+                } else {
+                    return None;
+                }
+
+                return Some(legal_moves);
+            }
+            _ => panic!("{:?}", self.board[position as usize]),
+            // Piece::Queen => /*...*/,
+        }
+
+        /* let (file, rank) = (position[0], position[1]);
+        match &self {
+            // this would look better if you just subtracted by 8 instead
+            Piece::King(_colour) => {
+                let mut legal_moves: Vec<String> = vec![];
+                if (rank - 1) * 8 + file > 0 {
+                    legal_moves.push(format!("<{}><{}>", file, rank - 1));
+
+                    if (rank - 1) * 8 + (file - 1) > (rank - 1) * 8 {
+                        legal_moves.push(format!("<{}><{}>", file - 1, (rank - 1)));
+                    }
+
+                    if (rank - 1) * 8 + (file + 1) < (rank - 1) * 8 + 8 {
+                        legal_moves.push(format!("<{}><{}>", file + 1, (rank - 1)));
+                    }
+                }
+
+                if (rank + 1) * 8 + file < 8 * 8 + file {
+                    legal_moves.push(format!("<{}><{}>", file, rank + 1));
+
+                    if (rank + 1) * 8 + (file - 1) > (rank + 1) * 8 {
+                        legal_moves.push(format!("<{}><{}>", file - 1, (rank + 1)));
+                    }
+
+                    if (rank + 1) * 8 + (file + 1) < (rank + 1) * 8 + 8 {
+                        legal_moves.push(format!("<{}><{}>", file + 1, (rank + 1)));
+                    }
+                }
+
+                if rank * 8 + (file - 1) > rank * 8 {
+                    legal_moves.push(format!("<{}><{}>", file - 1, rank));
+                }
+                if rank * 8 + (file + 1) < rank * 8 + 8 {
+                    legal_moves.push(format!("<{}><{}>", file + 1, rank));
+                } else {
+                    return None;
+                }
+
+                return Some(legal_moves);
+            }
+            _ => None,
+            // Piece::Queen => /*...*/,
+        } */
+    }
 }
 
 /// Implement print routine for Game.
@@ -265,24 +394,23 @@ mod tests {
         assert_eq!(bpawn.get_colour(), Colour::Black);
     }
 
-    /* #[test]
+    #[test]
     fn legal_move_king() {
         let game = Game::new();
 
         let placeholder = true;
 
-        let king = Piece::King(crate::Colour::White);
+        // let king = Piece::King(crate::Colour::White);
 
-        let king_moves = king.get_possible_moves("<3><0>").unwrap();
+        let king_moves = game.get_possible_moves("e1");
 
         println!("{:?}", king_moves);
 
         assert_eq!(placeholder, true)
-    } */
+    }
 
     #[test]
-    fn try_notation(){
-        println!("{:?}",Game::convert_notation("e1e2"));
-        
+    fn try_notation() {
+        println!("{:?}", Game::convert_notation("e1e2"));
     }
 }
